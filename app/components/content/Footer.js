6f86debefcea42499fcbbeae1210f3e02791c340
louisyungkang <hongyi0220@gmail.com>
2020-11-04 15:11:43 -0600

    Edit footer and its link

--- app/components/content/Footer.js
+++ app/components/content/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () =>
         <div className='footer'>
-            <div className='name-wrapper'><a  target='_blank' href='http://yungilhong.com'>yungil</a></div>
+            <div className='name-wrapper'><a  target='_blank' href='http://hongyi0220.github.io'>louis</a></div>
             <hr/>
             <div className='github-wrapper'><a  target='_blank' href='https://github.com/hongyi0220/bookish'>project repo</a></div>
         </div>;
